b3ac8bc9bf0e9383918ed575a9e5794d1bf41528
Caio Alves <j4c.2@hotmail.com>
2023-12-08 16:04:50 -0300

    Pop up de imagem

--- script.js
+++ script.js
@@ -26,3 +26,39 @@ function redirect() {
         '_blank'
     ) 
 }
+
+/* Image POP UP */
+
+function showPopup(imageSrc) {
+    // Verifica se a div do popup já existe
+    var popup = document.getElementById('popup');
+    
+    // Cria a div do popup dinamicamente se não existir
+    if (!popup) {
+      popup = document.createElement('div');
+      popup.id = 'popup';
+      document.body.appendChild(popup);
+
+      var popupImage = document.createElement('img');
+      popupImage.src = imageSrc;
+      popupImage.alt = 'Imagem ampliada';
+      popup.appendChild(popupImage);
+    } else {
+      // Se a div do popup já existir, atualiza a imagem
+      var popupImage = popup.querySelector('img');
+      popupImage.src = imageSrc;
+    }
+
+    // Exibe o popup ajustando a propriedade display para 'flex'
+    popup.style.display = 'flex';
+  }
+
+  document.body.addEventListener('click', function (event) {
+    // Oculta o popup se o usuário clicar fora da imagem
+    if (event.target.id !== 'popup' && !event.target.closest('.card')) {
+      var popup = document.getElementById('popup');
+      if (popup) {
+        popup.style.display = 'none';
+      }
+    }
+  });
